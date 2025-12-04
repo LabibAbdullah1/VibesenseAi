98abc39f0981b3ab3376c8e63378f41ba9ca4fd2
Leni Wahyuni Comel <wahyunileni61@gmail.com>
2025-12-04 22:02:09 +0700

    route web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestimonyController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\MoodResultController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +18,21 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth','is_admin'])->group(function (){
+    Route::get('/admin', function (){
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+
+    Route::get('/admin/users', function (){
+        return view('admin.users');
+    })->name('admin.user');
+    
+    Route::get('/admin/moods', function (){
+        return view('admin.moods');
+    })->name('admin.moods');
+
 });
 
 require __DIR__.'/auth.php';
